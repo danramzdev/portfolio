f3d132fc58d26db808848107ed45d8a78d90f1dd
Daniel Ramirez Mendoza <danramzdev@hotmail.com>
2018-12-24 16:21:44 -0600

    Add: .env al proyecto

--- config/database.php
+++ config/database.php
@@ -2,14 +2,17 @@
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+$dotenv = new Dotenv\Dotenv(__DIR__ . '/..');
+$dotenv->load();
+
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'cv_database',
-    'username'  => 'root',
-    'password'  => '',
+    'driver'    => getenv('DB_DRIVER'),
+    'host'      => getenv('DB_HOST'),
+    'database'  => getenv('DB_NAME'),
+    'username'  => getenv('DB_USERNAME'),
+    'password'  => getenv('DB_PASSWORD'),
     'charset'   => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix'    => '',
